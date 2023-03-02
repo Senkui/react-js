f21c79cefb2825a1029e90123e41780571b7e414
Senkui <senkuiz@163.com>
2023-03-02 21:24:24 +0800

    echarts 其他页面

---
+++ src/components/Bar.js
@@ -0,0 +1,41 @@
+import * as echarts from 'echarts'
+import { useEffect, useRef } from 'react'
+
+function Bar ({ title, xData, yData, style }) {
+  const domRef = useRef()
+  const chartInit = () => {
+    const myChart = echarts.init(domRef.current)
+    
+    // 绘制图表
+    myChart.setOption({
+      title: {
+        text: title
+      },
+      tooltip: {},
+      xAxis: {
+        data: xData
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: yData
+        }
+      ]
+    })
+  }
+
+  useEffect(()=>{
+    chartInit()
+  })
+  
+  return (
+    <div>
+      {/* 挂载节点 */}
+      <div ref={domRef} style={style}></div>
+    </div>
+  )
+}
+
+export default Bar
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,21 @@
-
-
+import './index.scss'
+import Bar from '@/components/Bar.js'
 
 function Home() { 
   return (
-    <div>111</div>
+    <div>
+    <Bar
+      title='主流框架满意度'
+      xData={['react', 'vue', 'angular']}
+      yData={[30, 40, 50]}
+      style={{ width: '500px', height: '400px' }} />
+    <Bar
+      title='主流框架使用度'
+      xData={['react', 'vue', 'angular']}
+      yData={[60, 70, 80]}
+      style={{ width: '300px', height: '200px' }} />
+  </div>
   )
 }
-
 
 export default Home
